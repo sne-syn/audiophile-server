aab5d5e6f640e4dc14cca986530ce1f9df060950
sne_syn <sss@anadeainc.com>
2022-09-12 09:47:27 +0200

    Build

--- index.ts
+++ index.ts
@@ -9,5 +9,5 @@ app.use("/assets", express.static("assets"));
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
