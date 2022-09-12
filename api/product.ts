b319dc7a7301667f8276499fcc43cfd6d29f4e81
sne_syn <sss@anadeainc.com>
2022-09-12 20:28:55 +0200

    Add product route

---
+++ api/product.ts
@@ -0,0 +1,36 @@
+import prisma from "../lib/prisma";
+const express = require("express");
+const router = express.Router();
+const cors = require("cors");
+
+router.get("/", cors(), async (req: any, res: any) => {
+  try {
+    const products = await prisma.product.findMany();
+
+    res.json({ products });
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+router.get("/:id", cors(), async (req: any, res: any) => {
+  try {
+    const product = await prisma.product.findUnique({
+      where: {
+        id: req.params.id,
+      },
+      include: {
+        category: true,
+        others: true,
+        image: true,
+        includes: true,
+      },
+    });
+
+    res.json({ product });
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+module.exports = router;
--- index.ts
+++ index.ts
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
 const category = require("./api/category");
+const product = require("./api/product");
 
 app.use(express.json({ extended: false }));
 app.use("/api/category", category);
+app.use("/api/product", product);
 app.use("/assets", express.static("assets"));
 
 const PORT = process.env.PORT || 5000;
